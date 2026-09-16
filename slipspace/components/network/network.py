4ec127b2941cedecd3416454a531b5db5e523712
Muqtxdir <muqtxdir@gmail.com>
2026-09-17 03:49:09 +0530

    feat(network): add `LanRow` and `WifiRow`

--- slipspace/components/network/network.py
+++ slipspace/components/network/network.py
@@ -8,6 +8,8 @@ import gi
 
 gi.require_version("AstalNetwork", "0.1")
 
+from gettext import gettext as _
+
 from gi.repository import AstalNetwork, GObject
 
 WIRED_ICON = "network-wired-symbolic"
@@ -21,10 +23,21 @@ class Network(GObject.Object):
     icon_name = GObject.Property(type=str, default=OFFLINE_ICON)
     available = GObject.Property(type=bool, default=False)
 
+    has_wifi = GObject.Property(type=bool, default=False)
+    wifi_icon_name = GObject.Property(type=str, default=WIFI_ICON)
+    wifi_enabled = GObject.Property(type=bool, default=False)
+    wifi_subtitle = GObject.Property(type=str, default="")
+
+    wired_connected = GObject.Property(type=bool, default=False)
+    wired_icon_name = GObject.Property(type=str, default=WIRED_ICON)
+    wired_subtitle = GObject.Property(type=str, default="")
+
     def __init__(self, network: AstalNetwork.Network = None, **kwargs):
         super().__init__(**kwargs)
 
         self._wifi = None
+        self._wired = None
+        self._enabled_binding = None
         self._network = network if network is not None else AstalNetwork.get_default()
 
         if self._network is None:
@@ -34,10 +47,12 @@ class Network(GObject.Object):
             self._network.connect(signal, self._on_changed)
 
         self._watch_wifi()
+        self._watch_wired()
         self._update()
 
     def _on_changed(self, _object, _pspec):
         self._watch_wifi()
+        self._watch_wired()
         self._update()
 
     def _watch_wifi(self) -> None:
@@ -46,13 +61,38 @@ class Network(GObject.Object):
         if wifi is self._wifi:
             return
 
+        if self._enabled_binding is not None:
+            self._enabled_binding.unbind()
+            self._enabled_binding = None
+
         self._wifi = wifi
 
         if wifi is None:
             return
 
-        wifi.connect("notify::icon-name", self._on_changed)
-        wifi.connect("notify::enabled", self._on_changed)
+        for signal in ("notify::icon-name", "notify::enabled", "notify::ssid"):
+            wifi.connect(signal, self._on_changed)
+
+        self._enabled_binding = wifi.bind_property(
+            "enabled",
+            self,
+            "wifi-enabled",
+            GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
+        )
+
+    def _watch_wired(self) -> None:
+        wired = self._network.props.wired
+
+        if wired is self._wired:
+            return
+
+        self._wired = wired
+
+        if wired is None:
+            return
+
+        for signal in ("notify::icon-name", "notify::internet", "notify::state"):
+            wired.connect(signal, self._on_changed)
 
     def _has_device(self) -> bool:
         return (
@@ -60,7 +100,7 @@ class Network(GObject.Object):
             or self._network.props.wired is not None
         )
 
-    def _wifi_enabled(self) -> bool:
+    def _wifi_is_enabled(self) -> bool:
         wifi = self._network.props.wifi
 
         return wifi is not None and wifi.props.enabled
@@ -88,7 +128,42 @@ class Network(GObject.Object):
 
         return OFFLINE_ICON
 
+    def _update_wifi(self) -> None:
+        wifi = self._network.props.wifi
+
+        self.props.has_wifi = wifi is not None
+
+        if wifi is None:
+            return
+
+        self.props.wifi_icon_name = wifi.props.icon_name or WIFI_ICON
+
+        if not wifi.props.enabled:
+            self.props.wifi_subtitle = _("Off")
+        elif wifi.props.ssid:
+            self.props.wifi_subtitle = wifi.props.ssid
+        else:
+            self.props.wifi_subtitle = _("Not connected")
+
+    def _update_wired(self) -> None:
+        wired = self._network.props.wired
+        connected = (
+            wired is not None
+            and wired.props.internet == AstalNetwork.Internet.CONNECTED
+        )
+
+        self.props.wired_connected = connected
+
+        if wired is None:
+            return
+
+        self.props.wired_icon_name = wired.props.icon_name or WIRED_ICON
+        self.props.wired_subtitle = _("Connected") if connected else _("Not connected")
+
     def _update(self) -> None:
+        self._update_wifi()
+        self._update_wired()
+
         if not self._has_device():
             self.props.icon_name = OFFLINE_ICON
             self.props.available = False
@@ -97,5 +172,5 @@ class Network(GObject.Object):
         self.props.icon_name = self._icon()
         self.props.available = (
             self._network.props.primary != AstalNetwork.Primary.UNKNOWN
-            or self._wifi_enabled()
+            or self._wifi_is_enabled()
         )
