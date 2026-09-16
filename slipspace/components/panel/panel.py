c34f2d4824b66295abea41b93ac662e618e52291
Muqtxdir <muqtxdir@gmail.com>
2026-09-17 05:00:04 +0530

    feat: integrate `PowerProfilesIcon` into `Panel`

--- slipspace/components/panel/panel.py
+++ slipspace/components/panel/panel.py
@@ -16,6 +16,7 @@ from slipspace.components.bluetooth import BluetoothIcon
 from slipspace.components.datetime import DatetimeLabel
 from slipspace.components.microphone import MicrophoneIcon
 from slipspace.components.network import NetworkIcon
+from slipspace.components.powerprofiles import PowerProfilesIcon
 from slipspace.components.speaker import SpeakerIcon
 
 GObject.type_ensure(BatteryIcon)
@@ -23,9 +24,12 @@ GObject.type_ensure(BluetoothIcon)
 GObject.type_ensure(DatetimeLabel)
 GObject.type_ensure(MicrophoneIcon)
 GObject.type_ensure(NetworkIcon)
+GObject.type_ensure(PowerProfilesIcon)
 GObject.type_ensure(SpeakerIcon)
 
 
 @Gtk.Template(resource_path="/com/muqtxdir/slipspace/components/panel/panel.ui")
 class Panel(Adw.Bin):
     __gtype_name__ = "Panel"
+
+    show_settings = GObject.Property(type=bool, default=False)
