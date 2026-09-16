6d88df8b4df39b8990c8044bc30daa6b28c9b686
Muqtxdir <muqtxdir@gmail.com>
2026-09-17 05:08:27 +0530

    feat(quicksettings): add `QuickSettings` overlay-view

---
+++ slipspace/components/quicksettings/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+#
+# Copyright 2026 Muqtadir
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from slipspace.components.quicksettings.quicksettings import QuickSettings
+
+__all__ = ["QuickSettings"]
