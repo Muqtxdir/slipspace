5c8d47757e33b61892a60610947a1d4aedca89e6
Muqtxdir <muqtxdir@gmail.com>
2026-09-17 05:09:08 +0530

    feat(quicksettings): integrate `QuickSettings` into window

--- slipspace/window.py
+++ slipspace/window.py
@@ -7,15 +7,25 @@
 from gi.repository import Adw, GObject, Gtk
 
 from slipspace.components.panel import Panel
+from slipspace.components.quicksettings import QuickSettings
 
 GObject.type_ensure(Panel)
+GObject.type_ensure(QuickSettings)
 
 
 @Gtk.Template(resource_path="/com/muqtxdir/slipspace/window.ui")
 class SlipspaceWindow(Adw.ApplicationWindow):
     __gtype_name__ = "SlipspaceWindow"
 
-    label = Gtk.Template.Child()
+    panel = Gtk.Template.Child()
+    quick_settings = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        self.panel.bind_property(
+            "show-settings",
+            self.quick_settings,
+            "show-settings",
+            GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
+        )
