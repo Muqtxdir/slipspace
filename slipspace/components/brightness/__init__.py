430887d5c37bac8decf900178681e6009fd25c10
Muqtxdir <muqtxdir@gmail.com>
2026-09-17 04:39:59 +0530

    feat(brightness): add `BrightnessRow`  via `astal-brightness`

---
+++ slipspace/components/brightness/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+#
+# Copyright 2026 Muqtadir
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from slipspace.components.brightness.brightness import Brightness
+from slipspace.components.brightness.brightness_row import BrightnessRow
+
+__all__ = ["Brightness", "BrightnessRow"]
